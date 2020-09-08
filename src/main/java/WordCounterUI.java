8838cd115aaf2946622ec1809d1144fa09c9a28c
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-09-08 12:44:46 +0200

    add user interface decoupled from domain

---
+++ src/main/java/WordCounterUI.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class WordCounterUI {
+    private WordCounter wordCounter = new WordCounter();
+
+    public static void main(String[] args) {
+        WordCounterUI wordCounterInterface = new WordCounterUI();
+        wordCounterInterface.countWordsFromCLI();
+    }
+
+    private void countWordsFromCLI() {
+        String text = readTextFromCLI();
+        int wordCount = wordCounter.countWords(text);
+        System.out.println("Number of words: " + wordCount);
+    }
+
+    private String readTextFromCLI() {
+        String text;
+        System.out.println("Enter text: ");
+        Scanner scanner = new Scanner(System.in);
+        text = scanner.nextLine();
+        return text;
+    }
+}
