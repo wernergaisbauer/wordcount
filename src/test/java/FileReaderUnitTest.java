c112ec6db5a428edf277f4c9e591f731a004bc91
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-09-09 08:33:59 +0200

    add file reader unit test

---
+++ src/test/java/FileReaderUnitTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class FileReaderUnitTest {
+    @Test
+    public void TestReadText() {
+        String text = FileReader.readFile("./src/test/resources/mytextfortest.txt");
+        assertEquals("Mary had\na little\nlamb", text);
+    }
+
+    @Test
+    public void TestReadTextFail() {
+        String text = FileReader.readFile("./src/test/resources/abc.txt");
+        assertNull(text);
+    }
+}
