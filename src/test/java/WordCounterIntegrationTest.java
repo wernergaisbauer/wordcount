f10f2bac2f4c87205608720e7c16be822a2b320c
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-09-09 09:40:47 +0200

    add test and text file

--- src/test/java/WordCounterIntegrationTest.java
+++ src/test/java/WordCounterIntegrationTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 public class WordCounterIntegrationTest {
 
     @Test
-    public void TestMainMethod() {
+    public void TestMainMethodCountWordsFromCLI() {
         InputStream sysInBackup = System.in;
         ByteArrayInputStream bais = new ByteArrayInputStream("Mary had a little lamb".getBytes());
         System.setIn(bais);
@@ -25,5 +25,18 @@ public class WordCounterIntegrationTest {
         System.setOut(sysOutBackup);
         String s = new String(baos.toByteArray(), Charset.defaultCharset());
         assertEquals("Enter text: \n" + "Number of words: 4\n", s);
+    }
+
+    @Test
+    public void TestMainMethodCountWordsFromFile() {
+        PrintStream old = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(baos);
+        System.setOut(out);
+        WordCounterUI.main(new String[] {"mytext.txt"});
+        System.out.flush();
+        System.setOut(old);
+        String s = new String(baos.toByteArray(), Charset.defaultCharset());
+        assertEquals("Number of words: 4\n", s);
     }
 }
