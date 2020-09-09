d22ae87338a156895a11f9e9b22912224d3bf183
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-09-09 12:07:39 +0200

    change rules for word definition and split (hypen are no longer separated)

--- src/main/java/WordCounterBasic.java
+++ src/main/java/WordCounterBasic.java
@@ -1,13 +1,13 @@
 import java.util.regex.Pattern;
 
 public class WordCounterBasic implements WordCounter {
-    protected static final String wordDefinition = "[a-zA-Z]+";
+    protected static final String wordDefinition = "[a-zA-Z-]+";
 
     public String[] getSplitStrings(String input) {
         if (input == null || input.isEmpty()) {
             return null;
         }
-        return input.split("[\\s.-]+");
+        return input.split("[\\s.]+");
     }
 
     public int countWords(String text) {
