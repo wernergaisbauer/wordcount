3dbb1b27cd3ac9786df2ab8b8ebbdb41adcea844
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-09-09 08:25:27 +0200

    improve test

--- src/test/java/WordCounterBasicUnitTest.java
+++ src/test/java/WordCounterBasicUnitTest.java
@@ -4,35 +4,40 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WordCounterBasicUnitTest {
-    private WordCounterBasic wordCounterBasic;
+    private WordCounter wordCounter;
 
     @Before
     public void init() {
-        wordCounterBasic = new WordCounterBasic();
+        wordCounter = new WordCounterBasic();
     }
 
     @Test
     public void TestWithFiveWords() {
-        assertEquals(5, wordCounterBasic.countWords("Mary had a little lamb"));
+        assertEquals(5, wordCounter.countWords("Mary had a little lamb"));
     }
 
     @Test
     public void TestWithEmptyString() {
-        assertEquals(0, wordCounterBasic.countWords(""));
+        assertEquals(0, wordCounter.countWords(""));
     }
 
     @Test
     public void TestWithSingleCharacters() {
-        assertEquals(5, wordCounterBasic.countWords("a b c d e     $     ?"));
+        assertEquals(5, wordCounter.countWords("a b c d e     $     ?"));
     }
 
     @Test
     public void TestWithSingleCharactersAndSpacesAround() {
-        assertEquals(5, wordCounterBasic.countWords("     a b c d e     $     ?    "));
+        assertEquals(5, wordCounter.countWords("     a b c d e     $     ?    "));
     }
 
     @Test
     public void TestWithNonWords() {
-        assertEquals(0, wordCounterBasic.countWords("73374 23782"));
+        assertEquals(0, wordCounter.countWords("73374 23782"));
+    }
+
+    @Test
+    public void TestWithWordsAndNonWords() {
+        assertEquals(2, wordCounter.countWords("James Bond 73374 23782"));
     }
 }
